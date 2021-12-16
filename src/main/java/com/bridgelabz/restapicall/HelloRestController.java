5250d9165eab27d1ac74124fdc33f2c1af019733
mudable <mudableshilpa@gmail.com>
2021-12-16 18:47:59 +0530

    Added : UC4 used PUT method to pass the parameter

--- src/main/java/com/bridgelabz/restapicall/HelloRestController.java
+++ src/main/java/com/bridgelabz/restapicall/HelloRestController.java
@@ -2,10 +2,15 @@ package com.bridgelabz.restapicall;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.bridgelabz.restapicall.controller.User;
 
 @RestController
 @RequestMapping("/hello")
@@ -13,6 +18,8 @@ import org.springframework.web.bind.annotation.RestController;
  * @RestController:to create the rest controller class.
  * @RequestMapping:describe the URL.
  * @RequestParam:to pass the value as parameter.
+ * @GetMapping:to get the name as a parameter.
+ * @PostMpping:to post the first name and last name as parameter
  */
 public class HelloRestController {
 	@RequestMapping(value = { "", "/", "/home" })
@@ -27,4 +34,13 @@ public class HelloRestController {
 	    public String sayHelloParam(@PathVariable String name) {
 	        return "Hello " + name + " From Bridgelabz";
 	    }
+
+		@PostMapping("/post")
+		public String postData(@RequestBody User user) {
+			return "Hello " + user.getFirstName() + " " + user.getLastName();	
+		}
+		@PutMapping("/put/{firstname}")
+		public String putData(@PathVariable String firstname, @RequestParam(value="lastname") String lasttname) {
+			return "Hello " + firstname +" "+ lasttname;
+		}
 }
