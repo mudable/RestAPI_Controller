13efe9c9e1e48dc28317c2d7cff68f7505bbcadf
mudable <mudableshilpa@gmail.com>
2021-12-16 17:11:16 +0530

    Added : UC3 to Use GET Request Method and pass name as

--- src/main/java/com/bridgelabz/restapicall/HelloRestController.java
+++ src/main/java/com/bridgelabz/restapicall/HelloRestController.java
@@ -1,5 +1,7 @@
 package com.bridgelabz.restapicall;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -20,5 +22,9 @@ public class HelloRestController {
 	 @RequestMapping(value={"/query"}, method=RequestMethod.GET)
 	    public String sayHello(@RequestParam(value="name") String name) {
 	        return "Hello" + name + "From Bridgelabz";
+	    }
+	  @GetMapping("/param/{name}")
+	    public String sayHelloParam(@PathVariable String name) {
+	        return "Hello " + name + " From Bridgelabz";
 	    }
 }
