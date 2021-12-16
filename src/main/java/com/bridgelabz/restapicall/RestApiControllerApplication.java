4a7db3a072f9e0b6e0ba4e221257d70c5fa4fe7b
mudable <mudableshilpa@gmail.com>
2021-12-16 16:52:53 +0530

    crated master branch

---
+++ src/main/java/com/bridgelabz/restapicall/RestApiControllerApplication.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.restapicall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiControllerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiControllerApplication.class, args);
+	}
+
+}
